e1926820852edb09240ed4128ed6808fe8109863
mklewandowski <matt.kain.lewandowski@gmail.com>
2024-02-12 17:40:38 -0600

    Add additional calls to TMDB API

--- src/App.js
+++ src/App.js
@@ -11,12 +11,47 @@ function App() {
     getTrendingMovieData("movie");
   }, []);
 
+  useEffect(() => {
+    getMovieDetails("movie", "238");
+  }, []);
+
   async function getTrendingMovieData(type) {
     try {
       let resp = await axios.get(`https://api.themoviedb.org/3/trending/${type}/day?api_key=${apiKey}&media_type=movie`);
-      console.log(21, resp.data.results);
+      console.log(resp.data.results);
 
       setMovieData(resp.data.results);
+    } catch (error) {
+
+    }
+  }
+
+  async function getTopRatedMovieData(type) {
+    try {
+      let resp = await axios.get(`https://api.themoviedb.org/3/${type}/top_rated?api_key=${apiKey}&media_type=movie`);
+      console.log(resp.data.results);
+
+      setMovieData(resp.data.results);
+    } catch (error) {
+
+    }
+  }
+
+  async function getNowPlayingMovieData(type) {
+    try {
+      let resp = await axios.get(`https://api.themoviedb.org/3/${type}/now_playing?api_key=${apiKey}`);
+      console.log(resp.data.results);
+
+      setMovieData(resp.data.results);
+    } catch (error) {
+
+    }
+  }
+
+  async function getMovieDetails(type, id) {
+    try {
+      let resp = await axios.get(`https://api.themoviedb.org/3/${type}/${id}?api_key=${apiKey}`);
+      console.log(resp.data);
     } catch (error) {
 
     }
@@ -33,10 +68,16 @@ function App() {
             Trending Movies
           </button>
           <button onClick={() => {
-            getTrendingMovieData("tv");
+            getTopRatedMovieData("movie");
           }
           }>
-            Trending TV
+            Top Rated Movies
+          </button>
+          <button onClick={() => {
+            getNowPlayingMovieData("movie");
+          }
+          }>
+            Now Playing Movies
           </button>
         </div>
         <div className='flex-container'>
